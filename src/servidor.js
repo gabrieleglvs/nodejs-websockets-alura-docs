c1d417275539625a5dac89f1930bd336a19aa7d2
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2024-04-20 20:00:05 -0300

    servindo arquivos da pasta public de forma estatica

--- src/servidor.js
+++ src/servidor.js
@@ -1,6 +1,14 @@
 import express from "express";
+import url from "url";
+import path from "path";
 
 const app = express();
 const porta = process.env.porta || 3000;
+
+//exatamente desde a pasta raiz até onde estamos
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../../", "public");
+//lendo os arquivos de public de forma estática
+app.use(express.static(diretorioPublico));
 
 app.listen(porta, () =>  console.log(`Servidor escutando na porta ${porta}`))
