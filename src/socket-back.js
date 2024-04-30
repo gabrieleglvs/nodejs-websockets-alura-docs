cf51ef28cfe57e3313029f9d93f978d184be52c4
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2024-04-30 17:30:23 -0300

    emitindo evento de texto no front e capturando no back

--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,9 @@ import io from "./servidor.js";
 
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou! ID:", socket.id);
-})
+
+  //capturando evento que está sendo emitido do front
+  socket.on("texto_editor", (textoEditorValue) => {
+    console.log(textoEditorValue);
+  });
+});
