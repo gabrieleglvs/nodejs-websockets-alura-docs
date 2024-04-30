5b38e835795ffc11794fb72782b7c7ea8d90a729
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2024-04-30 17:47:43 -0300

    emitindo, capturando e exibindo mensagens ao cliente

--- public/documento.js
+++ public/documento.js
@@ -3,5 +3,11 @@ const socket = io();
 const textoEditor = document.getElementById("editor-texto");
 
 textoEditor.addEventListener("keyup", () => {
+  ////emitindo evento do front para o back
   socket.emit("texto_editor", textoEditor.value);
+})
+
+//capturando no front, evento que está sendo emitido no back
+socket.on("texto_editor_clientes", (textoEditorValue) => {
+  textoEditor.value = textoEditorValue;
 })
--- src/socket-back.js
+++ src/socket-back.js
@@ -3,8 +3,10 @@ import io from "./servidor.js";
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou! ID:", socket.id);
 
-  //capturando evento que está sendo emitido do front
+  //capturando no back evento que está sendo emitido do front
   socket.on("texto_editor", (textoEditorValue) => {
-    console.log(textoEditorValue);
+    //emitindo evento do back para o front
+    //socket.broadcast > emite para todos os clientes menos p/ ele msm
+    socket.broadcast.emit('texto_editor_clientes', textoEditorValue);
   });
 });
